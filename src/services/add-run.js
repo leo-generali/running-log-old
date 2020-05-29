7bdaafa81b548bc5262c2909e4df885a77205774
Leo Generali <leogen17@gmail.com>
2020-05-29 11:32:05 -0400

    Add run creator node script

--- src/services/add-run.js
+++ src/services/add-run.js
@@ -1,10 +1,12 @@
 const inquirer = require("inquirer");
 const chalk = require("chalk");
 const moment = require("moment");
-const fs = require("fs");
+const fse = require("fs-extra");
 
 const filters = require("./filters");
 const shoes = require("../site/_data/shoes");
+
+const ACTIVITY_DIR = "./src/site/activities";
 
 const questions = [
   // Date
@@ -33,7 +35,9 @@ const questions = [
     name: "time",
     message: "How long was the effort?",
     validate: function (value) {
-      const pass = value.match(/(?:[01]\d|2[0-3]):(?:[0-5]\d):(?:[0-5]\d)/);
+      const pass = value.match(
+        /^(?:(?:([01]?\d|2[0-3]):)?([0-5]?\d):)?([0-5]?\d)$/
+      );
       if (pass) return true;
       return "Please enter a valid time.";
     },
@@ -47,10 +51,21 @@ const questions = [
       "What was the pace? We estimated a pace based off what you told us. Please correct if wrong.",
     default: function (answers) {
       const hmsArray = answers.time.split(":");
-      const seconds = +hmsArray[0] * 60 * 60 + +hmsArray[1] * 60 + +hmsArray[2];
+      let seconds = 0;
+
+      // If there are no hours in the time value
+      if (hmsArray.length === 2) {
+        seconds = +hmsArray[0] * 60 + +hmsArray[1];
+      }
+
+      // If there are hours in the time value
+      if (hmsArray.length === 3) {
+        seconds = +hmsArray[0] * 60 * 60 + +hmsArray[1] * 60 + +hmsArray[2];
+      }
+
       const pace = seconds / answers.distance;
 
-      console.log(filters.formatSeconds(pace));
+      return filters.formatSeconds(pace);
     },
   },
 
@@ -70,23 +85,56 @@ const questions = [
 ];
 
 function main() {
-  console.log("Please add the details about your run:");
+  console.log(chalk.bgBlue.whiteBright(" Welcome! ⚡"));
+  console.log(
+    chalk.bgBlue.whiteBright(" Please add the details about your run: ")
+  );
 
+  // Ask the users question from command line
   inquirer.prompt(questions).then((answers) => {
     console.log("An activity with the following details has been created:");
+
+    // Create all the front matter for the activity
     const frontMatter = createFrontMatter(answers);
+
+    // Get the eventual path of the file
+    const path = `${ACTIVITY_DIR}/${activityDateToPath(answers.date)}.md`;
+
+    // Create the file and subsequent directories
+    fse.outputFile(path, frontMatter, (err) => {
+      if (err) {
+        console.log(chalk.bgRed.whiteBright(` ${err} `));
+        return;
+      }
+
+      console.log(
+        `Succesfully created a new activity with the following details: 👍`
+      );
+      console.log(JSON.stringify(answers, null, 4));
+    });
   });
 }
 
 function createFrontMatter(answers) {
-  return `---
-  date: ${answers.data}
+  return `
+---
+date: ${answers.date}
 
-  time: ${answers.time}
-  distance: ${answers.distance}
-  pace: ${answers.pace}
-  type: easy
-  ---`;
+time: ${answers.time}
+distance: ${answers.distance}
+pace: ${answers.pace}
+type: easy
+
+shoeId: ${answers.shoeId}  
+---
+
+CONTENT HERE
+
+`.trim();
+}
+
+function activityDateToPath(date) {
+  return moment(date, "YYYY-MM-DD").format("YYYY/M/D");
 }
 
 main();
